1db4f49920e30cad2cba92de73614c0d9214f728
Brett Weaver <brett@weav.net>
2012-04-25 09:49:44 -0700

    updating bootstrap script output

--- scripts/rhel6/bootstrap-rhel6.sh
+++ scripts/rhel6/bootstrap-rhel6.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chef_version='0.10.8'
+bootstrap_log='/tmp/bootstrap.log'
 
 # Must be run as root
 if [ `whoami` != "root" ]; then
@@ -24,6 +25,9 @@ function status {
 let current_operation=0
 let total_operations=`egrep ^status $0 | wc -l`
 
+# Getting started
+status "Log file located at $bootstrap_log"
+
 # Change umask to something sane
 status 'Setting umask to something sane'
 umask 022
@@ -33,19 +37,19 @@ done
 
 # Update the system
 status 'Running Yum Update'
-yum -q -y update
+yum -q -y update >> $bootstrap_log 2>&1
 
 # Install Dependencies
 status 'Installing Chef Dependencies'
-yum -q -y install gcc cpp libgcc
+yum -q -y install gcc cpp libgcc make >> $bootstrap_log 2>&1
 if [ $? -ne 0 ]; then
-  echo "Error Installing Build Essentials"
+  echo 'Error Installing Chef Dependencies'
   exit 1
 fi
 
 # Install Ruby
 status 'Installing Ruby'
-yum -q -y install ruby rubygems ruby-devel
+yum -q -y install ruby rubygems ruby-devel >> $bootstrap_log 2>&1
 if [ $? -ne 0 ]; then
   echo "Error Installing Chef"
   exit 1
@@ -53,7 +57,7 @@ fi
 
 # Install Chef
 status 'Installing Chef'
-gem install chef -v $chef_version --no-ri --no-rdoc
+gem install chef -v $chef_version --no-ri --no-rdoc >> $bootstrap_log 2>&1
 if [ $? -ne 0 ]; then
   echo "Error Installing Chef"
   exit 1
@@ -62,3 +66,7 @@ fi
 # Create Chef Log Directory
 status 'Creating Chef Log Directory'
 mkdir -p -m 700 /var/log/chef
+
+# Complete
+echo 'Bootstraping completed.'
+echo 'To apply base role, run: chef-solo -c config/solo.rb -j nodes/default.json'
