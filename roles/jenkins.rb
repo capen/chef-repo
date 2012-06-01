0aab00182a53307078834218e7a2e8844508c0a0
Brett Weaver <brett@weav.net>
2012-06-01 12:12:15 -0700

    moving iptables last so it is not restarted by jenkins

--- roles/jenkins.rb
+++ roles/jenkins.rb
@@ -1,7 +1,7 @@
 name 'jenkins'
 description 'Single Jenkins Box'
-run_list 'recipe[intu-baseline-tweaks]', 'recipe[iptables]', 
-         'recipe[jenkins]', 'recipe[jenkins::proxy_apache2]'
+run_list 'recipe[intu-baseline-tweaks]', 'recipe[jenkins]',
+         'recipe[jenkins::proxy_apache2]', 'recipe[iptables]'
 
 override_attributes(
   "iptables" => {
