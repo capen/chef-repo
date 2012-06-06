077f883a52fa53b7084f792e74477fd198d4b068
Brett Weaver <brett@weav.net>
2012-06-06 13:45:50 -0700

    updated iptables

--- roles/baseline.rb
+++ roles/baseline.rb
@@ -1,3 +1,9 @@
 name 'baseline'
 description 'Prepare chef and apply Intuit tweaks'
 run_list 'recipe[intu-baseline-tweaks]'
+
+override_attributes(
+  "iptables" => {
+    "enabled" => false
+  }
+)
